4539add24f457701f5e1295a45a11f78d0e974be
tkPun <ewinatkpun@gmail.com>
2021-03-07 02:49:27 -0500

    reorganized repo

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,150 +1,95 @@
-const answersTrackerContainer = document.querySelector(".answers-tracker")
-const options = document.querySelector(".options").children
-const questionNumberSpan = document.querySelector(".question-num-value")
-const question=document.querySelector(".question")
-const totalQuestionsSpan =document.querySelector(".total-questions")
-const correctAnswersSpan =document.querySelector(".correct-answers")
-const totalQuestionsSpan2 =document.querySelector(".total-questions2")
-const percentageSpan =document.querySelector(".percentage")
 
-let currentIndex;
-let index = 0;
-let answeredQuestions =[]; // array of anwered question indexes
-let score = 0;
 
-const opt1 = document.querySelector(".option1")
-const opt2 = document.querySelector(".option2")
-const opt3 = document.querySelector(".option3")
-const opt4 = document.querySelector(".option4")
 
-const questions = [
-    {
-        'id': '1',
-        'question':'What superpower do you want to have?',
-        'options': ['Flying', 'Invisibility', 'Shape-shifting','Mind reading'],
-    },
-    {
-        'id': '2',
-        'question':"Where would you like to explore the most?",
-        'options': ['A snow mountain', 'A dark forest', 'A sunny waterfront', 'Underwater coral reef'],
-    },
-    {
-         'id': '3',
-         'question':"Which of these would your friends describe you best?",
-         'options':['Shy', 'Outgoing', 'Curious', 'Funny'],
+
+var currentQuestion = 2; //
+
+
+
+function startQuiz() {
+        document.getElementById("brief").className = "invisible";
+        document.getElementById("question-1").className = "question";
+        var startBtn = document.getElementById('startQuizBtn');
+        startBtn.parentNode.removeChild(startBtn);
     }
-]
+	
+	
+	
 
-totalQuestionsSpan.innerHTML = questions.length
-
-function load(){
-    questionNumberSpan.innerHTML = index + 1
-    question.innerHTML = questions[currentIndex].question;
-    opt1.innerHTML = questions[currentIndex].options[0]
-    opt2.innerHTML = questions[currentIndex].options[1]
-    opt3.innerHTML = questions[currentIndex].options[2]
-    opt4.innerHTML = questions[currentIndex].options[3]
-    index++
-}
-
-//Check if selected answer is correct or wrong
-function check(element){
-    if(element.id == questions[currentIndex].answer){
-        element.className="correct"
-        updateAnswersTracker("correct")
-        score++
+function getNumberOfQuestions() {
+        
+        var totalQuestions = document.querySelectorAll('#quiz-questions .question').length;
+        return totalQuestions;
     }
-    else {
-        element.className="wrong"
-        updateAnswersTracker("wrong")
+	
+	
+
+
+function nextQuestion() {
+        hideQuestion(currentQuestion);
+        hideAnswerButton();
+        showQuestion(currentQuestion);
+        currentQuestion++;
     }
-    disableClick();
-}
+	
 
-//Make sure the user selected an item before clicking on the Next button
-function validate(){
-    if(!options[0].classList.contains("disabled")){
-        alert("Please select an option")
+	
+
+function setAnswerButton() {
+        document.getElementById("confirm_answer").className = "";
     }
-    else{
-        randomQuestion();
-        enableClick();
-    }
-}
 
-//Listener function for click event on Next button
-function next(){
-    validate();
-}
 
-//Function to disable click for the options
-function disableClick(){
-    for(let i=0; i<options.length; i++){
-        options[i].classList.add("disabled")
 
-        if(options[i].id == questions[currentIndex].answer){
-            options[i].classList.add('correct');
-        }
-    }
-}
 
-//Function to reanable click in the options
-function enableClick(){
-    for(let i=0; i<options.length; i++){
-        options[i].classList.remove("disabled", "correct", "wrong")
+function hideAnswerButton() {
+        document.getElementById("confirm_answer").className = "invisible";
+    }	
+	
+	
 
-    }
-}
 
-//Function to select a random question
-function randomQuestion(){
-    let randomNumber = Math.floor(Math.random()*questions.length);
-    if(index == questions.length){
-        quizOver();
-    }
-    else{
-        if(answeredQuestions.length > 0){
-            if(answeredQuestions.includes(randomNumber)){
-                randomQuestion();
-            }
-            else {
-                currentIndex = randomNumber;
-                load();
+function hideQuestion(id) {
+        var totalQuestions = getNumberOfQuestions();
+        for (var i = 1; i <= totalQuestions; i++) {
+            if (i !== id) {
+                document.getElementById("question-" + i).className = "question invisible";
             }
         }
-        if(answeredQuestions.length == 0){
-            currentIndex = randomNumber
-            load()
+    }
+
+
+	
+
+function showQuestion(id) {
+        var totalQuestions = getNumberOfQuestions();
+        if (id <= totalQuestions) {
+            document.getElementById("question-" + id).className = "question";
+        } else {
+			setEndingSentence() //begins the end screen process if id is above total question
         }
-        //add the question to list of anwered questions
-        answeredQuestions.push(randomNumber)
     }
-}
 
-//Restart the quiz
-window.onload=function(){
-    this.randomQuestion();
-    this.answersTracker();
-}
 
-//Set up answers tracker elements
-function answersTracker(){
-    for(let i=0; i< questions.length; i++){
-        const div =document.createElement("div")
-        answersTrackerContainer.appendChild(div);
+
+
+function getEndingSentence() {
+    var quizRadio = document.getElementsByName("rq");
+	var content = ''; //It's easier to handle if we simply merge all sentences into a string
+    for (var i = 0; i < quizRadio.length; i++) {
+        if (quizRadio[i].checked) {
+            content += quizRadio[i].getAttribute("data-endingsentence"); //these are the attributes used to generate quiz answers
+        }
     }
+	return content;
 }
 
-//Update the answers tracker elements
-// function updateAnswersTracker(newClass){
-//     answersTrackerContainer.children[index -1].classList.add(newClass)
-// }
 
-//Displays the quiz-over page if quiz is over
-function quizOver(){
-    document.querySelector(".quiz-over").classList.add("show")
-}
 
-function enterhomepage(){
-    window.location.reload();
-}
+
+function setEndingSentence() {
+	var personalityResults = getEndingSentence();
+    document.getElementById("results_screen").className = "";
+	
+
+} 
