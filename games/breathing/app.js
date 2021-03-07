73d9b0184bfdd78de752d0e83faae02e1feea6ff
rupali-talele <rupali.talele1995@gmail.com>
2021-03-06 23:40:54 -0800

    breathing exercise

--- games/breathing/app.js
+++ games/breathing/app.js
@@ -1,47 +1,47 @@
+window.onload = function()
+{
+    var timer = document.querySelector(".timer");
+    timer.innerHTML = "1 mins 0 secs";
+    var st = document.getElementsByClassName("start")
+    console.log("DOM LOADED")
+        // declare variables for star icons
+    const smile = document.querySelectorAll(".smile");
+    // stars list
+    var smilesList = document.querySelectorAll(".smiles li");
+    // // close icon in modal
+    // var closeicon = document.querySelector(".close");
+    // declare modal
+    var modal = document.getElementById("popup1") 
+var second = 0, minute = 1; hour = 0;
+var timer = document.querySelector(".timer");
+var play = document.getElementById("play-again")
+ 
+}
+var interval;   
 
-
-// declare variables for star icons
-const smile = document.querySelectorAll(".smile");
-
- // stars list
- let smilesList = document.querySelectorAll(".smiles li");
-
- // close icon in modal
- let closeicon = document.querySelector(".close");
-
- // declare modal
- let modal = document.getElementById("popup1")
-
-document.body.onload = startGame();
-
-
-// @description function to start a new play
 function startGame(){
 
+    console.log("startgame")
     // reset rating
-    for (var i= 0; i < smile.length; i++){
-        smile[i].style.visibility = "visible";
-    }
+    // for (var i= 0; i < smile.length; i++){
+    //     smile[i].style.visibility = "visible";
+    // }
     //reset timer
     second = 0;
     minute = 1;
-    var timer = document.querySelector(".timer");
-    timer.innerHTML = "1 mins 0 secs";
+    var st = document.getElementById("start")
+    st.style.display = "block"
     clearInterval(interval);
     startTimer();
 }
 
-// @description game timer
-var second = 0, minute = 1; hour = 0;
-var timer = document.querySelector(".timer");
-var interval;
 function startTimer(){
     var timer = document.querySelector(".timer");
     interval = setInterval(function(){
         timer.innerHTML = minute+" mins "+second+" secs";
         if(second == 0){
             minute--;
-            second=60;
+            second=3;
         }
         second--;
         if(second == 0 && minute == 0){
@@ -56,41 +56,50 @@ function congratulations(){
     clearInterval(interval);
 
     // show congratulations modal
-    let modal = document.getElementById("popup1")
-    modal.classList.add("show");
-
+    var modal = document.getElementById("modal")
+    modal.style.display = "block"
     // declare star rating variable
     var smileRating = document.querySelector(".smiles").innerHTML;
 
     //showing move, rating, time on modal
-    document.getElementById("smileRating").innerHTML = smileRating;
+    // document.getElementById("smileRating").innerHTML = smileRating;
 
     //closeicon on modal
-    closeModal();
+    // closeModal();
+    
+
+    
 
 }
 
-
-// @description close icon on modal
-function closeModal(){
-    closeicon.addEventListener("click", function(e){
-        modal.classList.remove("show");
-        startGame();
-    });
+function closemodal()
+{
+    var modal = document.getElementById("modal")
+    modal.style.display = "none"
 }
+
+
+// // @description close icon on modal
+// function closeModal(){
+//     closeicon.addEventListener("click", function(e){
+//         modal.classList.remove("show");
+//         startGame();
+//     });
+// }
 
 
 // @desciption for user to play Again
 function playAgain(){
-    modal.classList.remove("show");
+    // modal.classList.remove("show");
+    closemodal()
     startGame();
 }
 
 
 // loop to add event listeners to each card
-for (var i = 0; i < cards.length; i++){
-    card = cards[i];
-    card.addEventListener("click", displayCard);
-    card.addEventListener("click", cardOpen);
-    card.addEventListener("click",congratulations);
-};
+// for (var i = 0; i < cards.length; i++){
+//     card = cards[i];
+//     card.addEventListener("click", displayCard);
+//     card.addEventListener("click", cardOpen);
+//     card.addEventListener("click",congratulations);
+// };
